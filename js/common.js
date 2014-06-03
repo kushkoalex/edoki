565bab26bf9eb7990bab90bb22125390b8b7ea47
Alexander Kushko <kushko.alex@gmail.com>
2014-06-03 11:35:41 +0300

    refactoring

--- js/common.js
+++ js/common.js
@@ -3,9 +3,7 @@ $(function () {
     var isOrdered = false;
     $(".fancy").fancybox({ hideOnContentClick: true, showCloseButton: false, padding: 0, margin: 0, opacity: false });
 
-
     $(".order-button").click(function () {
-
         $(".order-text").fadeOut();
         $(".order-phone-input").fadeIn().focus();
         $(".enter-phone-number-text").fadeIn();
@@ -36,18 +34,19 @@ $(function () {
 
     $(".order-link").click(function () {
         var phone = document.getElementsByClassName("order-phone-input")[0].value;
-        console.log(phone);
-
-        var data = {};
-        data.phone = phone;
-        data.from = document.getElementById("fromPage").value;
-        console.log(data);
+        if(phone=='')
+            phone = document.getElementsByClassName("order-phone-input")[1].value;
+        var from  = document.getElementById("fromPage").value;
+        var order = {};
+        order.phone = phone;
+        order.from = from;
+        console.log(order);
         $.ajax({
-            url: "/api/makeorder/" + phone,
+            url: "/api/makeorder",
             contentType: "application/json",
             accepts: "application/json",
             type: "POST",
-            data: data,
+            data: JSON.stringify(order),
             success: function () {
                 $(".order-button").fadeOut();
                 $(".order-link").fadeOut();
@@ -57,19 +56,20 @@ $(function () {
                 alert("Спасибо за заказ!\r\n Едоки свяжутся с Вами в ближайшее время :)");
                 isOrdered = true;
             },
-            error: function (err) {
+            error: function () {
                 $(".order-button").fadeOut();
                 $(".order-link").fadeOut();
                 $(".enter-phone-number-text").fadeOut();
                 $(".check-map").fadeOut();
                 alert("Ошибка при отправке запроса");
-                console.log(err);
-                for (var prop in err) {
-                    console.log(prop);
-                }
+//                console.log(err);
+//                for (var prop in err) {
+//                    console.log(prop);
+//                }
                 isOrdered = true;
             }
         });
     });
+
 
 });
--- matafuku/js/main.js
+++ matafuku/js/main.js
@@ -1,8 +1,5 @@
 $(function () {
 
-    var isOrdered = false;
-    $(".fancy").fancybox({ hideOnContentClick: true, showCloseButton: false, padding: 0, margin: 0, opacity: false });
-
 
     var u;
     var dc = $(".dish-component");
@@ -111,12 +108,7 @@ $(function () {
     setActiveComponent(firstElement);
 
 
-    $(".order-button").click(function () {
-        $(".order-text").fadeOut();
-        $(".order-phone-input").fadeIn().focus();
-        $(".enter-phone-number-text").fadeIn();
-        $(".check-map").fadeIn();
-    });
+
 
     $("#btnMakeDinner").click(function () {
         //hideControls();
@@ -192,69 +184,14 @@ $(function () {
                     });
                 }
             });
+
     });
 
 
-    $(".order-phone-input").inputmask("(999)999-99-99", {
-        oncomplete: function () {
-            $(".order-link").fadeIn();
-            $(".enter-phone-number-text").fadeIn();
-            $(".check-map").fadeIn();
-        },
-        oncleared: function () {
-            $(".order-link").fadeOut();
-            $(".check-map").fadeOut();
-        },
-        onincomplete: function () {
-            $(".order-link").fadeOut();
-            $(".order-phone-input").fadeOut();
-            $(".order-text").fadeIn();
-            $(".enter-phone-number-text").fadeOut();
-            $(".check-map").fadeOut();
-        },
-        "clearIncomplete": true
-    });
 
 
-    $(".order-link").click(function () {
-        var phone = document.getElementsByClassName("order-phone-input")[0].value;
-        if(phone=='')
-            phone = document.getElementsByClassName("order-phone-input")[1].value;
-//        console.log(phone);
 
-        var data = {};
-        data.phone = phone;
-        data.from = document.getElementById("fromPage").value;
-//        console.log(data);
-        $.ajax({
-            url: "/api/makeorder/" + phone,
-            contentType: "application/json",
-            accepts: "application/json",
-            type: "POST",
-            data: data,
-            success: function () {
-                $(".order-button").fadeOut();
-                $(".order-link").fadeOut();
-                $(".enter-phone-number-text").fadeOut();
-                $(".check-map").fadeOut();
-                //$(".phone span").show(0);
-                alert("Спасибо за заказ!\r\n Едоки свяжутся с Вами в ближайшее время :)");
-                isOrdered = true;
-            },
-            error: function (err) {
-                $(".order-button").fadeOut();
-                $(".order-link").fadeOut();
-                $(".enter-phone-number-text").fadeOut();
-                $(".check-map").fadeOut();
-                alert("Ошибка при отправке запроса");
-                console.log(err);
-                for (var prop in err) {
-                    console.log(prop);
-                }
-                isOrdered = true;
-            }
-        });
-    });
+
 
     //mixComponents();
 
